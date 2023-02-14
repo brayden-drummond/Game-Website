dd06c75b763f7cd5cc0d1ff892414c1cece711d8
Brayden Drummond <107587496+brayden-drummond@users.noreply.github.com>
2023-02-15 07:24:32 +1300

    working on board-box

--- tic-tac-toe/src/components/board.tsx
+++ tic-tac-toe/src/components/board.tsx
@@ -6,3 +6,19 @@ import { Box } from './board-box'
 import { Storage } from '../storage/storage'
 
 import * as utils from '../utils/functions'
+
+export class Board extends React.Component {
+  constructor(props: any) {
+    super(props)
+
+    // initial component state on render
+
+    this.state = {
+      boxes: Array(9).fill(null),
+      history: [],
+      xTurn: true
+    }
+  };
+
+  storage = new Storage();
+}
