3e9119e236d480210db24df32f2467d91de771f9
Brayden Drummond <107587496+brayden-drummond@users.noreply.github.com>
2023-02-24 10:58:41 +1300

    start on home page setup

---
+++ home/src/App.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import './App.css';
+import Banner from './components/Banner';
+
+function App() {
+  return (
+    <div className="App">
+      <Banner />
+      <h1>App</h1>
+    </div>
+  );
+}
+
+export default App;
---
+++ home/src/components/Banner.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+import styles from '../styles/Banner.module.scss'
+
+export default function Banner() {
+  return (
+    <nav className={styles.banner}>
+      <h1>Welcome</h1>
+    </nav>
+  )
+}
