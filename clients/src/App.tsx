51828cac61537a9ea2a71935650781cc2d8650a9
Brayden Drummond <107587496+brayden-drummond@users.noreply.github.com>
2023-02-24 14:13:18 +1300

    navbar working

---
+++ clients/src/App.tsx
@@ -0,0 +1,17 @@
+import React from 'react';
+
+import Banner from './components/Banner';
+
+
+function App() {
+  return (
+    <div className="App">
+      <div>
+        <Banner />
+      </div>
+      <h1>Home Page</h1>
+    </div>
+  );
+}
+
+export default App;
