28fb6ead37d44ffa88031e263bc1c97cb3ecf52e
Brayden Drummond <107587496+brayden-drummond@users.noreply.github.com>
2023-02-28 19:58:39 +1300

    game container start

--- clients/src/components/Games.tsx
+++ clients/src/components/Games.tsx
@@ -1,19 +1,29 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
-
+import { gamesData } from '../games-data/games-data'
 
 import styles from '../styles/Games.module.scss'
-import TicTacToe from '../tic-tac-toe/Tic-Tac-Toe'
-
-const backgroundVideo = require('../media/background.mp4')
 
 export default function Games() {
   return (
     <>
      <div>
       <Link to="/">Home</Link>
-      <Link to="/tictactoe">Tic Tac Toe</Link>
       </div>
+      <div className={styles.container}>
+        {gamesData.map((game, i) => {
+          return (
+            <div key={i} className={styles.gameContainer}>
+              <Link to={game.path}>
+                <img className={styles.image} src={game.image} alt={game.name} />
+              </Link>
+              <p className={styles.name}>{game.name}</p>
+            </div>
+          )
+        })}
+      </div>
+    
+        
     </>
   )
 }
---
+++ clients/src/games-data/games-data.ts
@@ -0,0 +1,9 @@
+export const gamesData = [
+  {name: 'Tic Tac Toe', image: 'https://tse4.mm.bing.net/th?id=OIP.2tBScaNEJFL0a7Hus03xKQHaHa&pid=Api&P=0', path: '/tictactoe'},
+  {name: 'Tic Tac Toe', image: 'https://tse4.mm.bing.net/th?id=OIP.2tBScaNEJFL0a7Hus03xKQHaHa&pid=Api&P=0', path: '/tictactoe'},
+  {name: 'Tic Tac Toe', image: 'https://tse4.mm.bing.net/th?id=OIP.2tBScaNEJFL0a7Hus03xKQHaHa&pid=Api&P=0', path: '/tictactoe'},
+  {name: 'Tic Tac Toe', image: 'https://tse4.mm.bing.net/th?id=OIP.2tBScaNEJFL0a7Hus03xKQHaHa&pid=Api&P=0', path: '/tictactoe'},
+  {name: 'Tic Tac Toe', image: 'https://tse4.mm.bing.net/th?id=OIP.2tBScaNEJFL0a7Hus03xKQHaHa&pid=Api&P=0', path: '/tictactoe'},
+  {name: 'Tic Tac Toe', image: 'https://tse4.mm.bing.net/th?id=OIP.2tBScaNEJFL0a7Hus03xKQHaHa&pid=Api&P=0', path: '/tictactoe'},
+  {name: 'Tic Tac Toe', image: 'https://tse4.mm.bing.net/th?id=OIP.2tBScaNEJFL0a7Hus03xKQHaHa&pid=Api&P=0', path: '/tictactoe'}
+]
