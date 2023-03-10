b54cf472e165c5bffc9c14ef2564e42f0814980a
Brayden Drummond <107587496+brayden-drummond@users.noreply.github.com>
2023-03-11 09:22:55 +1300

    set up weather app

--- clients/src/App.tsx
+++ clients/src/App.tsx
@@ -4,6 +4,7 @@ import { Routes, Route, Link } from 'react-router-dom'
 import Banner from './components/Banner';
 import Home from './components/Home';
 import TicTacToe from './tic-tac-toe/Tic-Tac-Toe';
+import WeatherApp from './weather-app/Weather-app';
 
 
 function App() {
@@ -16,6 +17,7 @@ function App() {
       </div>
       <Routes>
         <Route path='/tictactoe' element={<TicTacToe />} />
+        <Route path='/weather' element={<WeatherApp />} />
         <Route path='/' element={<Home />} />
       </Routes>
     
---
+++ clients/src/weather-app/Weather-app.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+
+function WeatherApp() {
+  return (
+    <div className="App">
+      <h2>Weather</h2>
+    
+    </div>
+  );
+}
+
+export default WeatherApp;
