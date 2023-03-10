40d7ae5d020bab82717a45327d1fb634e1c1a660
Brayden Drummond <107587496+brayden-drummond@users.noreply.github.com>
2023-03-11 09:37:54 +1300

    pre weather start commit

--- clients/src/games-data/games-data.ts
+++ clients/src/games-data/games-data.ts
@@ -6,7 +6,7 @@ export interface GamesData {
 
 export const gamesData: GamesData[] = [
   {name: 'Tic Tac Toe', image: 'https://tse4.mm.bing.net/th?id=OIP.2tBScaNEJFL0a7Hus03xKQHaHa&pid=Api&P=0', path: '/tictactoe'},
-  {name: 'Tic Tac Toe', image: 'https://tse4.mm.bing.net/th?id=OIP.2tBScaNEJFL0a7Hus03xKQHaHa&pid=Api&P=0', path: '/tictactoe'},
+  {name: 'Weather App', image: 'https://www.pngfind.com/pngs/m/691-6916213_stormy-weather-icon-icon-hd-png-download.png', path: '/weather'},
   {name: 'Tic Tac Toe', image: 'https://tse4.mm.bing.net/th?id=OIP.2tBScaNEJFL0a7Hus03xKQHaHa&pid=Api&P=0', path: '/tictactoe'},
   {name: 'Tic Tac Toe', image: 'https://tse4.mm.bing.net/th?id=OIP.2tBScaNEJFL0a7Hus03xKQHaHa&pid=Api&P=0', path: '/tictactoe'},
   {name: 'Tic Tac Toe', image: 'https://tse4.mm.bing.net/th?id=OIP.2tBScaNEJFL0a7Hus03xKQHaHa&pid=Api&P=0', path: '/tictactoe'},
